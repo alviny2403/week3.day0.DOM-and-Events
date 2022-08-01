e1360590a1c584e011ce6d064f548742f77e47c8
allenslayz2 <50374252+allenslayz2@users.noreply.github.com>
2022-08-01 15:00:31 -0400

    addingStyle complete

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,7 @@
+let rightSideColor= document.querySelector(".right_side")
+rightSideColor.style.backgroundColor= "#d3c26f"
+
+let nameShadow= document.getElementsByClassName("name")
+for (let i= 0; i< nameShadow.length; i ++){
+    nameShadow[i].style.textShadow= "-6px 5px 2px yellow"
+}
