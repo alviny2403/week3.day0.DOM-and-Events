2dafbce6fd2496cd5233a8bbd5771da63cfdbab1
allenslayz2 <50374252+allenslayz2@users.noreply.github.com>
2022-08-01 21:49:08 -0400

    update

---
+++ BONUS_newChampion/lib/js/main.js
@@ -0,0 +1,1 @@
+document.querySelector(".main_container").innerHTML+= "<h1>Alvin</h1>"
---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,5 @@
+let changeName= document.querySelector("#item_name")
+changeName.innerHTML= "Frostfang"
+
+let changeImg= document.getElementById("item_img")
+changeImg.src= "assets/Frostfang_item.png"
