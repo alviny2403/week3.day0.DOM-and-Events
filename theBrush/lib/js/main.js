2e8c7435614f89843753023d33f2803eadee4862
allenslayz2 <50374252+allenslayz2@users.noreply.github.com>
2022-08-01 22:08:18 -0400

    finished theBrush

---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,7 @@
+let clicked= document.querySelector("body")
+let changeImg= document.getElementById("rengar")
+
+clicked.onclick=function(){
+    changeImg.src= "assets/rengar.png"
+    alert("You've been deleted!")
+}
